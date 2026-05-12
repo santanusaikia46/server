980891726fdc547ce8e14df075a8d229cc64db2b
SANTANU SAIKIA <saikiasantanu46@gmail.com>
2026-05-13 01:05:54 +0530

    feat: add nodemailer utility for sending system emails

--- utils/sendEmail.js
+++ utils/sendEmail.js
@@ -10,6 +10,7 @@ const sendEmail = async (options) => {
       user: process.env.SMTP_USER,
       pass: process.env.SMTP_PASS,
     },
+    family: 4, // Force IPv4 to avoid ENETUNREACH on IPv6
   });
 
   // Verify connection configuration
